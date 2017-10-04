da6b1cfbafad967ed55c824b9962ef0f28eaa51d
Sergey Obukhov <osergey@live.com>
2017-10-04 06:24:46 +0300

    Remove all non-primitive logic.

--- src/main/java/org/example/server/web/TestController.java
+++ src/main/java/org/example/server/web/TestController.java
@@ -1,11 +1,9 @@
 package org.example.server.web;
 
-import org.example.server.model.HelloRequest;
-import org.example.server.model.HelloResponse;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * Created by ronin on 13.09.2017.
+ * Created by osergey on 04.10.2017.
  */
 @RestController
 public class TestController {
@@ -16,12 +14,7 @@ public class TestController {
     }
 
     @GetMapping("/hello")
-    public HelloResponse hello(@RequestParam("name") String name) {
-        return new HelloResponse(name);
-    }
-
-    @PostMapping("/hello-body")
-    public HelloResponse hello(@RequestBody HelloRequest request) {
-        return new HelloResponse(request.getName());
+    public String hello(@RequestParam("name") String name) {
+        return "Hello " + name;
     }
 }
