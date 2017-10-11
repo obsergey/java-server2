e1e191c33bd77cc2190edf349f4e8df9217781a1
obsergey <32325802+obsergey@users.noreply.github.com>
2017-10-11 13:12:26 +0300

    Update TetsControllerTest.java

--- src/test/java/org/example/server/web/TetsControllerTest.java
+++ src/test/java/org/example/server/web/TetsControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-public class TestControllerTest {
+public class TetsControllerTest {
     TestController testController = new TestController();
 
     @Test
